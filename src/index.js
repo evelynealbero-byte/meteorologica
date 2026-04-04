f885a94e8260e94af555f5f749ac638fc99e6642
Evelyne Albero <evelyne.albero@gmail.com>
2026-04-04 11:18:25 +0200

    added apikey

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+function searchCity(city) {
+  let apikey = "a0bdfcad3964e6e3300b74o509fbat1b";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&unit=metric`;
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
