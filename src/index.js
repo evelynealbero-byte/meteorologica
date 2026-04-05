58fc830c3191186acda97c9c392735a3bc9ed7d4
Evelyne Albero <evelyne.albero@gmail.com>
2026-04-05 22:10:56 +0200

    add if minutes

--- src/index.js
+++ src/index.js
@@ -30,6 +30,11 @@ function formatDate(date) {
     "Saturday",
   ];
   let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
   return `${day} ${hours}: ${minutes}`;
 }
 
